801513668d94a9df823d424e251877f8da78f260
hyo-jae-jung <60522307+hyo-jae-jung@users.noreply.github.com>
2021-06-13 12:51:06 +0900

    more image

--- js/background.js
+++ js/background.js
@@ -2,6 +2,11 @@ const images = [
     "1.jpg",
     "2.jpg",
     "3.jpg",
+    "blackholexray.png",
+    "uranus_lg.jpeg",
+    "potw2119a.jpg",
+    "sunrise.jpg",
+    "halloween_sun.jpg"
 ];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)]
