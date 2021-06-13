ffd839c5c17e2ff63001df5f6ac320e9223eb9c0
hyo-jae-jung <60522307+hyo-jae-jung@users.noreply.github.com>
2021-06-13 14:07:52 +0900

    version 1.00

--- js/air.js
+++ js/air.js
@@ -2,6 +2,7 @@ const API_TOKEN = "09c2d111c28f7d23b016888f5b72634fdf8fca17"
 const MORE_INFO = "https://aqicn.org/scale/"
 const CITY = "seoul"
 const url = `https://api.waqi.info/feed/${CITY}/?token=${API_TOKEN}`
+const levelTable = "https://aqicn.org/scale/"
 
 fetch(url).then((response) => response.json()).then((data) => {
     const aqi = data.data.aqi;
